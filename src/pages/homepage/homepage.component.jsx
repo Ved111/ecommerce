f7c8a531c0ffaa1bd7960936179a02a417e38f3a
vedant <vedant.db99@gmail.com>
2020-02-24 15:11:27 +0530

    Updating the app with shop-page, collection-preview component and collection-item

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -6,7 +6,7 @@ import Directory from "../../components/directory/directory.component";
 
 const HomePage = ({ history }) => (
     <div clasName = 'homepage'>
-        <Directory />
+        <Directory history />
     </div>
 );
 
